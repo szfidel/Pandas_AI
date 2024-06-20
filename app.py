dbfee96e423dc2f166f69b6ee831dd745212a832
Fidel Serrano <s.fidelz@gmail.com>
2024-06-19 21:28:10 -0700

    Add app.py script to use pandas ai agent with streamlit

--- app.py
+++ app.py
@@ -1,27 +1,29 @@
-import os
 from pandasai.llm.local_llm import LocalLLM
-from pandasai import SmartDataframe
 import streamlit as st
 import pandas as pd
+import matplotlib.pyplot as plt
+import numpy as np
+import seaborn as sns
+from pandasai import Agent
 
 
 model = LocalLLM(
     api_base = "http://localhost:11434/v1",
-    model = "codellama:latest"
+    model = "llama3:latest"
 )
 
 st.title("Data analysis with PandasAI")
 
-uploaded_file = st.file_uploader("Upload a CSV file", type=['csv'])
+uploaded_file = st.sidebar.file_uploader("Upload a CSV file", type=['csv'])
 
 if uploaded_file is not None:
     data = pd.read_csv(uploaded_file)
     st.write(data.head(3))
 
-    df = SmartDataframe(data, config={"llm": model})
+    agent = Agent(data, config={"llm": model})
     prompt = st.text_area("Enter your prompt:")
 
     if st.button("Generate"):
         if prompt:
             with st.spinner("Generating response..."):
-                st.write(df.chat(prompt))
+                st.write(agent.chat(prompt))
